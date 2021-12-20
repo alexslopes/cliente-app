1bb9a7c1a6b653801422f434d9a670e0fc71b7ab
alexs <alexstlopes@gmail.com>
2021-12-20 18:51:59 -0300

    Criando e injetando o servico de clientes

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientesService } from 'src/app/clientes.service';
 import { Cliente } from '../cliente';
 
 @Component({
@@ -11,9 +12,8 @@ export class ClientesFormComponent implements OnInit {
   cliente: Cliente;
   nome: string = 'Fulano';
 
-  constructor() {
-    this.cliente = new Cliente();
-    this.cliente.nome = "Douglas";
+  constructor( private service: ClientesService ) {
+    this.cliente = service.getCliente();
   }
 
   ngOnInit(): void {

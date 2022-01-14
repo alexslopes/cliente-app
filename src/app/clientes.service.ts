a81724f0eda1ac365771fc033b57b1745af676bb
alexs <alexstlopes@gmail.com>
2022-01-13 23:55:07 -0300

    68. Criando o componente de listagem de clientes

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -14,10 +14,17 @@ export class ClientesService {
     return this.http.post<Cliente>('http://localhost:8081/api/clientes',cliente);
   }
 
-  getCliente() : Cliente {
-    let cliente : Cliente = new Cliente();
-    cliente.nome = 'Fulano de tal';
-    cliente.cpf = '8888888888';
-    return cliente;
+  /*getClientes() : Observable<Cliente[]> {
+    return null;
+  }*/
+
+  getClientes() : Cliente[] {
+    let cliente = new Cliente();
+    cliente.id = 1;
+    cliente.nome = 'Fulano';
+    cliente.dataCadastro = '18/04/2020';
+    cliente.cpf = '12345678900';
+    return [cliente];
   }
+
 }

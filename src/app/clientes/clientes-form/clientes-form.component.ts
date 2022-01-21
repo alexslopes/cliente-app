71494d3572d91b7899e1a8b0d2b34cfe68463dc1
alexs <alexstlopes@gmail.com>
2022-01-20 22:07:59 -0300

    Acrescentado rotas

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ClientesService } from 'src/app/clientes.service';
 import { Cliente } from '../cliente';
 
@@ -13,11 +14,16 @@ export class ClientesFormComponent implements OnInit {
   success: boolean = false;
   errors: String[];
 
-  constructor( private service: ClientesService ) {
+  constructor( private service: ClientesService,
+               private router: Router ) {
     this.cliente = new Cliente();
   }
 
   ngOnInit(): void {
+  }
+
+  voltarParaListagem() {
+    this.router.navigate(['/clientes-lista'])
   }
 
   clicar() {

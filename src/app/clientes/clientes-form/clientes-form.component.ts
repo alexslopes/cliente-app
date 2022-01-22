8fb86435dfd97f0ebaf1e9f445e3792de4b2bdfd
alexs <alexstlopes@gmail.com>
2022-01-21 22:53:07 -0300

    Criado service de atualizaçao e corrigido params

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { ClientesService } from 'src/app/clientes.service';
 import { Cliente } from '../cliente';
 
@@ -22,15 +23,17 @@ export class ClientesFormComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    let params = this.activatedRoute.params;
-    if(params && params.value && params.value.id) {
-      console.log(params.value.id);
-      this.id = params.value.id;
-      this.service.getClienteById(this.id)
-      .subscribe(
-        response => this.cliente = response,
-        errorresponse => this.cliente = new Cliente())
-    }
+    let params: Observable<Params> = this.activatedRoute.params;
+    params.subscribe( urlParams => {
+      this.id = urlParams['id'];
+      if(this.id) {
+        this.service.getClienteById(this.id)
+        .subscribe(
+          response => this.cliente = response,
+          errorresponse => this.cliente = new Cliente())
+      }
+
+    })
 
     console.log(this.cliente.nome)
   }
@@ -44,16 +47,25 @@ export class ClientesFormComponent implements OnInit {
   }
 
   onSubmit() {
+    if(this.id) {
+      this.service.atualizar(this.cliente)
+      .subscribe( response =>
+        {this.success = true;
+        this.errors = null;
+      }, errorResponse => {
+        this.errors = [ 'Erro ao atualizar o cliente.']
+      })
+    } else {
+
     this.service.salvar(this.cliente).subscribe(
       (response) => { this.success = true;
         this.errors = null;
         this.cliente = response;
-      }
-      , errorResponse => {
+      }, errorResponse => {
         this.success = false;
         this.errors = errorResponse.error.errors;
-      }
-    )
+      })
   }
+}
 
 }

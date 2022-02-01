07159ba86eeaca0ca363c4cc8208cf1a08c165f4
alexs <alexstlopes@gmail.com>
2022-01-31 22:08:19 -0300

    86. Mensagem de feedback no formulario de Serviços Prestados

--- src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -13,6 +13,8 @@ export class ServicoPrestadoFormComponent implements OnInit {
 
   clientes: Cliente[] = [];
   servico: ServicoPrestado;
+  success: boolean = false;
+  errors: String[];
 
   constructor(
     private clienteService: ClientesService,
@@ -31,7 +33,15 @@ export class ServicoPrestadoFormComponent implements OnInit {
     this.service
       .salvar(this.servico)
       .subscribe( reponse => {
-        console.log(reponse);
+        this.success = true;
+        this.errors = null;
+        this.servico = new ServicoPrestado();
+      }, errorResponse => {
+        this.success = false;
+        this.errors = errorResponse.error.errors;
+        ;
+        ;
+
       })
   }
 

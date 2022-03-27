9c4da981e56ef574153d765d28edb86a7fae2b4c
alexs <alexstlopes@gmail.com>
2022-03-26 22:03:01 -0300

    105. Cadastro de usuario no front-end

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { Usuario } from './usuario'
 
 @Component({
   selector: 'app-login',
@@ -12,9 +14,11 @@ export class LoginComponent implements OnInit {
   password: string;
   loginError: boolean;
   cadastrando: boolean;
+  mensagemSucesso: string;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
@@ -31,6 +35,19 @@ export class LoginComponent implements OnInit {
 
   cancelaCadastro(){
     this.cadastrando = false;
+  }
+
+  cadastrar() {
+    const usuario: Usuario = new Usuario();
+    usuario.username = this.username;
+    usuario.password = this.password;
+    this.authService.salvar(usuario).subscribe( response => {
+      this.mensagemSucesso = "Cadastro realizado com sucesso! Efetue o login.";
+      this.loginError = false;
+    }, error => {
+      this.loginError = true;
+      this.mensagemSucesso = null;
+    })
   }
 
 }
